2cfad5e1ec0a70e53440faec6b067fcb334190d2
unknown <lucas11souza97@gmail.com>
2023-04-06 22:27:15 -0400

    feat: Created improviments

---
+++ db.py
@@ -0,0 +1,17 @@
+stores = {}
+items = {}
+
+items2= {
+    1: {
+        "name": "Lucas",
+        "price": 17.99
+    },
+    2: {
+        "name": "Lucas",
+        "price": 17.99
+    },
+    3: {
+        "name": "Lucas",
+        "price": 17.99
+    }
+}
