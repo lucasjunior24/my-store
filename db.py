bcbb009b95408e2c01e5455f755604c091fd691b
unknown <lucas11souza97@gmail.com>
2023-04-11 23:09:48 -0400

    feat: update in schemas

--- db.py
+++ db.py
@@ -1,17 +1,3 @@
-stores = {}
-items = {}
+from flask_sqlalchemy import SQLAlchemy
 
-items2= {
-    1: {
-        "name": "Lucas",
-        "price": 17.99
-    },
-    2: {
-        "name": "Lucas",
-        "price": 17.99
-    },
-    3: {
-        "name": "Lucas",
-        "price": 17.99
-    }
-}
+db = SQLAlchemy()
--- schemas.py
+++ schemas.py
@@ -1,19 +1,26 @@
 from marshmallow import Schema, fields
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
-    
+
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
 
-class StoreSchema(Schema):
+class PlainStoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
 
-    
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(load_only=True, required=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainStoreSchema(), dump_only=True))
